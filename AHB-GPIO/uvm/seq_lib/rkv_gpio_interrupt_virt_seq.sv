2dd18d5f11847e5319d3d050f4489a5da867d6f5
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-25 15:32:21 +0800

    4 interrupt types were verified successfully

--- AHB-GPIO/uvm/seq_lib/rkv_gpio_interrupt_virt_seq.sv
+++ AHB-GPIO/uvm/seq_lib/rkv_gpio_interrupt_virt_seq.sv
@@ -13,11 +13,10 @@ class rkv_gpio_interrupt_virt_seq extends rkv_gpio_base_virtual_sequence;
         super.body();
         `uvm_info(get_type_name(), "Entered body...", UVM_LOW)
         repeat(20) begin
-            // pin_id = $urandom_range(0, 15);
             std::randomize(pin_id);
-            // high_level_interrupt(pin_id);
-            // low_level_interrupt(pin_id);
-            // rising_edge_interrupt(pin_id);
+            high_level_interrupt(pin_id);
+            low_level_interrupt(pin_id);
+            rising_edge_interrupt(pin_id);
             falling_edge_interrupt(pin_id);
         end
         `uvm_info(get_type_name(), "Exiting body...", UVM_LOW)
@@ -72,39 +71,27 @@ class rkv_gpio_interrupt_virt_seq extends rkv_gpio_base_virtual_sequence;
     endtask
 
     task rising_edge_interrupt(bit[3:0] id);
-        bit [15:0] pin_dr, pin_rd; 
-        bit [15:0] before_clear, after_clear, rising_rd, fall_rd, fall_clear;
+        bit [15:0] pin_dr, pin_rd;
+        bit [15:0] falling_check, rising_check, clear_check;
         set_rising_edge_interrupt(id);
+        generate_rising_edge(pin_dr, id);       
+        get_intstatus(rising_check, id);
+        compare_data(pin_dr, rising_check);
+        generate_falling_edge(pin_dr, id);
+        get_intstatus(falling_check, id);
+        compare_data(rising_check, falling_check);
         rgm.INTCLEAR.write(status, 1 << id);
-        pin_dr[id] = 1;
-        vif.drive_portin(pin_dr);
-        wait_cycles(3);
-        get_intstatus(before_clear, id);
-        rising_rd = before_clear;
-        `uvm_info(get_type_name(), $sformatf("pin:%d rising 0->1,status should be 1:%b",id, before_clear), UVM_LOW)
-        rgm.INTCLEAR.write(status, 1 << id);
-        get_intstatus(after_clear, id);
-        `uvm_info(get_type_name(), $sformatf("pin:%d keep 1 and do clear,status should be 0:%b",id, after_clear), UVM_LOW)
-        generate_rising_edge(id);
-        //create a falling edge
-        pin_dr[id] = 0;
-        vif.drive_portin(pin_dr);
-        wait_cycles(3);
-        get_intstatus(fall_rd, id);
-        `uvm_info(get_type_name(), $sformatf("pin:%d falling 1->0,status should keep 1:%b",id, fall_rd), UVM_LOW)        
-        rgm.INTCLEAR.write(status, 1 << id);
-        get_intstatus(fall_clear, id);
-        `uvm_info(get_type_name(), $sformatf("pin:%d falling 1->0 and do clear,status should be 0:%b",id, fall_clear), UVM_LOW)
-        compare_data(rising_rd, fall_rd);
+        get_intstatus(clear_check, id);
+        compare_data(clear_check, 0);
     endtask
     
     task falling_edge_interrupt(bit[3:0] id);
         bit [15:0] pin_dr, pin_rd;
         bit [15:0] falling_check, rising_check, clear_check;
         set_falling_edge_interrupt(id);
-        generate_falling_edge(id);
+        generate_falling_edge(pin_dr, id);       
         get_intstatus(falling_check, id);
-        generate_rising_edge(id);
+        generate_rising_edge(pin_dr, id);
         get_intstatus(rising_check, id);
         compare_data(falling_check, rising_check);
         rgm.INTCLEAR.write(status, 1 << id);
@@ -112,7 +99,7 @@ class rkv_gpio_interrupt_virt_seq extends rkv_gpio_base_virtual_sequence;
         compare_data(clear_check, 0);
     endtask
 
-    task generate_rising_edge(bit[3:0] id);
+    task generate_rising_edge(ref bit[15:0] pin_dr, bit[3:0] id);
         pin_dr[id] = 0;
         vif.drive_portin(pin_dr);
         wait_cycles(3);
@@ -121,7 +108,7 @@ class rkv_gpio_interrupt_virt_seq extends rkv_gpio_base_virtual_sequence;
         wait_cycles(3);
     endtask
 
-    task generate_falling_edge(bit[3:0] id);
+    task generate_falling_edge(ref bit[15:0] pin_dr, bit[3:0] id);
         pin_dr[id] = 1;
         vif.drive_portin(pin_dr);
         wait_cycles(3);
