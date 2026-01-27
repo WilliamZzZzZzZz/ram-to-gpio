60d5b9bcff25559c59ed74d6efdff9a46643d756
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-27 23:46:59 +0800

    the completed lower 8 bits masked-access verified successfully

--- AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
+++ AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
@@ -10,11 +10,10 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
 
     virtual task body();
         bit [31:0] addr, data;
-        rand bit [15:0] initial_val, write_val;
-        rand bit [7:0]  mask_byte;
         super.body();
         `uvm_info(get_type_name(), "Entered...", UVM_LOW)
         repeat(20) begin
+            //randomize inputs
             masked_access_protection();
         end
           
@@ -23,10 +22,15 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
 
     task masked_access_protection();
         bit type_sel = 0;   //0->lower, 1->upper
-        bit [15:0]  final_val, expected_val;
+        bit [15:0]  final_val, expected_val, initial_val, write_val;
         bit [31:0]  target_addr;
-        uvm_status_e status;
+        bit [7:0]  mask_byte;
+        uvm_status_e status;  
 
+        //randomize inputs
+        initial_val = $urandom_range(0, 16'hFFFF);
+        write_val   = $urandom_range(0, 16'hFFFF);
+        mask_byte   = $urandom_range(0, 8'hFF);
         //set expected value
         expected_val[7:0]   = (write_val[7:0] & mask_byte) | (initial_val[7:0] & ~mask_byte);
         expected_val[15:8]  = initial_val[15:8];
@@ -47,13 +51,13 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
         
         //do compare
         if(final_val == expected_val) begin
-            `uvm_info(get_type_name(), "every bits' masked-access PASSED!")
+            `uvm_info(get_type_name(), "every bits' masked-access PASSED!",UVM_LOW)
         end
         else begin
             `uvm_error(get_type_name(), $sformatf("FAILED: expected_val: 0x%04X, actual_val: 0x%04X", expected_val, final_val))
             for(int i = 0; i < 16; i++) begin
                if(final_val[i] != expected_val[i]) begin
-                    `uvm_info(get_type_name(), $sformatf("bit[%0d] mismatch, expected %0b, actual %0b", i, expected_val[i], final_val[i]))
+                    `uvm_info(get_type_name(), $sformatf("bit[%0d] mismatch, expected %0b, actual %0b", i, expected_val[i], final_val[i]), UVM_LOW)
                end 
             end
         end
