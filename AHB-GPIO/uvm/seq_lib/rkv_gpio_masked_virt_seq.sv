398106a3cdcc11a9c990911876da3e9e8951adab
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-27 23:22:19 +0800

    a completed lower 8 bits masked-access test

--- AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
+++ AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
@@ -10,10 +10,11 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
 
     virtual task body();
         bit [31:0] addr, data;
-        bit [3:0] pin_id;
+        rand bit [15:0] initial_val, write_val;
+        rand bit [7:0]  mask_byte;
         super.body();
         `uvm_info(get_type_name(), "Entered...", UVM_LOW)
-        repeat(1) begin
+        repeat(20) begin
             masked_access_protection();
         end
           
@@ -22,51 +23,46 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
 
     task masked_access_protection();
         bit type_sel = 0;   //0->lower, 1->upper
-        bit [15:0]  initial_val, write_val, final_val;
-        bit [7:0]   mask_byte;
+        bit [15:0]  final_val, expected_val;
         bit [31:0]  target_addr;
-        bit [31:0]  addr, data;
-
         uvm_status_e status;
 
-        initial_val = 16'hFFFF;
-        write_val   = 16'h0000;
-        mask_byte   = 8'h01;
-
-        `uvm_do_with(single_write, {addr == 16'h0000;
-                                    data == initial_val;})
-        
+        //set expected value
+        expected_val[7:0]   = (write_val[7:0] & mask_byte) | (initial_val[7:0] & ~mask_byte);
+        expected_val[15:8]  = initial_val[15:8];
+        //calculate address
         if(type_sel == 0) begin //lower-8bits
             target_addr = 32'h400 + (mask_byte << 2);   //0x400 + mask_byte*4
         end
         else begin              //uppper-8bits
             target_addr = 32'h800 + (mask_byte << 2);   //0x800 + mask_byte*4
         end
-
+        //process data
+        `uvm_do_with(single_write, {addr == RKV_ROUTER_REG_ADDR_DATA;
+                                    data == initial_val;})
         `uvm_do_with(single_write, {addr == target_addr;
                                     data == write_val;})
-        wait_cycles(2);
-        `uvm_do_with(single_read, {addr == 16'h0004;})
+        `uvm_do_with(single_read, {addr == RKV_ROUTER_REG_ADDR_DATAOUT;})
         final_val = single_read.data;
         
-        `uvm_info(get_type_name(), $sformatf("Initial Value: %b", initial_val), UVM_LOW)
-        `uvm_info(get_type_name(), $sformatf("Write Value:   %b", write_val), UVM_LOW)
-        `uvm_info(get_type_name(), $sformatf("Mask Byte:     %b", mask_byte), UVM_LOW)
-        `uvm_info(get_type_name(), $sformatf("Final Value:   %b", final_val), UVM_LOW)
-        //compare 0-bit
-        if(final_val[0] == 0) begin
-            `uvm_info(get_type_name(), "0-bit changed successfully!", UVM_LOW)
+        //do compare
+        if(final_val == expected_val) begin
+            `uvm_info(get_type_name(), "every bits' masked-access PASSED!")
         end
         else begin
-            `uvm_info(get_type_name(), "0-bit changed failed", UVM_LOW)
+            `uvm_error(get_type_name(), $sformatf("FAILED: expected_val: 0x%04X, actual_val: 0x%04X", expected_val, final_val))
+            for(int i = 0; i < 16; i++) begin
+               if(final_val[i] != expected_val[i]) begin
+                    `uvm_info(get_type_name(), $sformatf("bit[%0d] mismatch, expected %0b, actual %0b", i, expected_val[i], final_val[i]))
+               end 
+            end
         end
-        //compare 1~7 bits
-        if(final_val[1] == 1 && final_val[2] == 1 && final_val[3] == 1 && final_val[4] == 1 && final_val[5] == 1 && final_val[6] == 1 && final_val[7] == 1) begin
-            `uvm_info(get_type_name(), "1~7 bits keep 1 successfully!", UVM_LOW)
-        end
-        else begin
-            `uvm_info(get_type_name(), "1~7 bits keep 1 failed", UVM_LOW)
-        end
+        `uvm_info(get_type_name(), $sformatf("Initial Value:    %b", initial_val), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Write Value:      %b", write_val), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Mask Byte:        %b", mask_byte), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Expected Value:   %b", expected_val), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Final Value:      %b", final_val), UVM_LOW)
+
     endtask
 
 endclass
