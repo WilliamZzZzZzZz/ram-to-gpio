4bca33ab01090360733b14f0050407de02d5f0fe
WilliamZzz <zchenkb@connect.ust.hk>
2026-01-27 22:14:54 +0800

    simple lower 8 bits masked-access test successfully

--- AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
+++ AHB-GPIO/uvm/seq_lib/rkv_gpio_masked_virt_seq.sv
@@ -9,12 +9,14 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
     endfunction
 
     virtual task body();
+        bit [31:0] addr, data;
         bit [3:0] pin_id;
         super.body();
         `uvm_info(get_type_name(), "Entered...", UVM_LOW)
-        repeat(20) begin
+        repeat(1) begin
             masked_access_protection();
         end
+          
         `uvm_info(get_type_name(), "Exiting...", UVM_LOW)
     endtask
 
@@ -30,9 +32,9 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
         initial_val = 16'hFFFF;
         write_val   = 16'h0000;
         mask_byte   = 8'h01;
-        // mask_byte   = $urandom();
-        // type_sel    = $urandom();
-        rgm.DATA.write(status, initial_val);
+
+        `uvm_do_with(single_write, {addr == 16'h0000;
+                                    data == initial_val;})
         
         if(type_sel == 0) begin //lower-8bits
             target_addr = 32'h400 + (mask_byte << 2);   //0x400 + mask_byte*4
@@ -43,9 +45,14 @@ class rkv_gpio_masked_virt_seq extends rkv_gpio_base_virtual_sequence;
 
         `uvm_do_with(single_write, {addr == target_addr;
                                     data == write_val;})
-        `uvm_do_with(single_read, {addr == RKV_ROUTER_REG_ADDR_DATA;})
+        wait_cycles(2);
+        `uvm_do_with(single_read, {addr == 16'h0004;})
         final_val = single_read.data;
         
+        `uvm_info(get_type_name(), $sformatf("Initial Value: %b", initial_val), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Write Value:   %b", write_val), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Mask Byte:     %b", mask_byte), UVM_LOW)
+        `uvm_info(get_type_name(), $sformatf("Final Value:   %b", final_val), UVM_LOW)
         //compare 0-bit
         if(final_val[0] == 0) begin
             `uvm_info(get_type_name(), "0-bit changed successfully!", UVM_LOW)
